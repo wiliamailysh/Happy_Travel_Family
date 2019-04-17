cd950c214b808a86b9eef249d7bc569d291101d5
wiliamailysh <victor.douay+dev@gmail.com>
2019-04-17 13:56:32 +0200

    Add controller homepage, Routes modified & index page structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Use to create admin authentification first time
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-10.times do 
-    Tour.create!(name: Faker::FunnyName.name, 
-                locomotion: Faker::Job.field, 
+10.times do
+    Tour.create!(name: Faker::FunnyName.name,
+                locomotion: Faker::Job.field,
                 duration: Faker::Number.within(1..8),
-                place: Faker::Nation.capital_city, 
-                category: Faker::Restaurant.name, 
-                level: Faker::House.furniture, 
-                description: Faker::GreekPhilosophers.quote, 
+                place: Faker::Nation.capital_city,
+                category: Faker::Restaurant.name,
+                level: Faker::House.furniture,
+                description: Faker::GreekPhilosophers.quote,
                 code: Faker::IDNumber.invalid)
 end
