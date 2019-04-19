a5eb411cfffa2413fb53a5114345e015663a7004
wiliamailysh <victor.douay+dev@gmail.com>
2019-04-19 12:16:25 +0200

    [PROGRESS] CSS design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'tour/index'
+  get '/tours', to: 'tour#index', as: 'tour'
   get '/', to: 'homepage#index', as: 'root'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
