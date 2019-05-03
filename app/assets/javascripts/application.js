bc28c6d5a1d15fb3f38e58ad1358a765e71be36a
wiliamailysh <victor.douay+dev@gmail.com>
2019-05-03 19:20:14 +0200

    Javascript event mouseover with hidden description ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,34 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// Load DOM
+document.addEventListener("turbolinks:load", function() {
+
+  // alert('Hello');
+
+  let enSavoirPlus = Array.from(document.querySelectorAll('.trip__link'));
+  console.log(enSavoirPlus);
+  let tripDescription = Array.from(document.querySelectorAll('.trip__description'));
+
+  for (let i = 0; i < enSavoirPlus.length; i++) {
+    enSavoirPlus[i].addEventListener('mouseover', (e) => {
+      tripDescription[i].classList.remove("trip__description--hidden");
+      tripDescription[i].classList.add("trip__description--block");
+      // console.log(`Message ${i}`);
+    })
+    enSavoirPlus[i].addEventListener('mouseout', (e) => {
+      tripDescription[i].classList.remove("trip__description--block");
+      tripDescription[i].classList.add("trip__description--hidden");
+    })
+  }
+
+//   // enSavoirPlus.forEach((e) => {
+//   //     e.addEventListener("mouseover", (e, index) => {
+//   //         // alert('Click');
+//   //         tripDescription[index].classList.remove("trip__description--hidden");
+//   //         tripDescription[index].classList.add("trip__description--block");
+//   //       })
+//   //   })
+//
+});
