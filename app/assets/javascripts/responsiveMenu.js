e52ad1ad5f1c532281d7e62f3f471e8a54ad1454
wiliamailysh <victor.douay+dev@gmail.com>
2019-05-17 20:47:42 +0200

    [PROGRESS]

--- app/assets/javascripts/responsiveMenu.js
+++
@@ -1,8 +0,0 @@
-function displayResponsiveMenu() {
-  var x = document.getElementById("navbar");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
